686b8e00199b4192069f4d9e1673290018884026
Ubuntu <vlad.garkusha1999@gmail.com~>
2026-02-15 22:06:02 +0200

    4

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = 'MY_API';
+const API_KEY = '21399489-be148a7d0b86f7d0e4ef4262d';
 
 export async function getImagesByQuery(query) {
   const response = await axios.get(BASE_URL, {
